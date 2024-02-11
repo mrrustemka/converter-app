7d693440a0660810095a2a13bdb44ecfbd2a5598
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2024-02-11 19:32:23 +0400

    add-components-to-app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import React from "react";
+import Chart from "./components/Chart";
+import ChartButton from "./components/ChartButton";
+import Currency from "./components/Currency";
+import Header from "./components/Header";
+import Input from "./components/Input";
+import Keyboard from "./components/Keyboard";
+import Output from "./components/Output";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Input />
+      <Currency />
+      <Currency />
+      <Output />
+      <Keyboard />
+      <ChartButton />
+      <Chart />
+    </div>
+  );
 }
 
 export default App;
