1a9b57fe9c44e224869a319dd4e0bfcd9227b186
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2024-02-11 19:50:52 +0400

    add-redux

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+// import {createStore } from "redux";
+import { legacy_createStore as createStore } from "redux";
+import { Provider } from "react-redux";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
