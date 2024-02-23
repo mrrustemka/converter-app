0345ab0aaa9f11a77610f2682543d6ce1cc06229
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2024-02-23 21:15:48 +0400

    add-currencies-symbols

--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -15,19 +15,19 @@ function Currency({ type, defaultValue, gridArea }) {
       >
         <MenuItem value="USD">$ USD</MenuItem>
         <MenuItem value="EUR">€ EUR</MenuItem>
-        <MenuItem value="JPY">JPY</MenuItem>
-        <MenuItem value="GBP">GBP</MenuItem>
-        <MenuItem value="CNY">CNY</MenuItem>
-        <MenuItem value="AUD">AUD</MenuItem>
-        <MenuItem value="CAD">CAD</MenuItem>
-        <MenuItem value="CHF">CHF</MenuItem>
-        <MenuItem value="HKD">HKD</MenuItem>
-        <MenuItem value="SGD">SGD</MenuItem>
-        <MenuItem value="SEK">SEK</MenuItem>
-        <MenuItem value="KRW">KRW</MenuItem>
-        <MenuItem value="NOK">NOK</MenuItem>
-        <MenuItem value="NZD">NZD</MenuItem>
-        <MenuItem value="INR">INR</MenuItem>
+        <MenuItem value="JPY">¥ JPY</MenuItem>
+        <MenuItem value="GBP">£ GBP</MenuItem>
+        <MenuItem value="CNY">¥ CNY</MenuItem>
+        <MenuItem value="AUD">$ AUD</MenuItem>
+        <MenuItem value="CAD">$ CAD</MenuItem>
+        <MenuItem value="CHF">₣ CHF</MenuItem>
+        <MenuItem value="HKD">$ HKD</MenuItem>
+        <MenuItem value="SGD">S$ SGD</MenuItem>
+        <MenuItem value="SEK">kr SEK</MenuItem>
+        <MenuItem value="KRW">₩ KRW</MenuItem>
+        <MenuItem value="NOK">kr NOK</MenuItem>
+        <MenuItem value="NZD">$ NZD</MenuItem>
+        <MenuItem value="INR">₹ INR</MenuItem>
       </Select>
     </Box>
   );
